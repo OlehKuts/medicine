a12d0d2f76031c82e9a969efbcb305644a1b9e2f
OlehKuts <43850889+OlehKuts@users.noreply.github.com>
2020-05-13 17:33:31 +0300

    Add files via upload

---
+++ build/precache-manifest.fd76ed127710c394cfcb1671ebfa955a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "189b2011b7736acfdb89318da9f7bcd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb906fa9f50b85f1342",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "99319d19c63d676f120b",
+    "url": "/static/css/main.e6481940.chunk.css"
+  },
+  {
+    "revision": "adb906fa9f50b85f1342",
+    "url": "/static/js/2.893bdd5f.chunk.js"
+  },
+  {
+    "revision": "99319d19c63d676f120b",
+    "url": "/static/js/main.0c1e1c6f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
